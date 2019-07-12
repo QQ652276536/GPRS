6c8fb72190fdb93e768b42d2fce774f1768db806
QQ652276536 <652276536@qq.com>
2019-07-12 16:28:18 +0800

    更改登录验证

--- app/src/main/java/com/example/blowdown_app/UserFragment.java
+++ app/src/main/java/com/example/blowdown_app/UserFragment.java
@@ -32,25 +32,19 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link UserFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link UserFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class UserFragment extends Fragment implements View.OnClickListener, View.OnFocusChangeListener {
+public class UserFragment extends Fragment implements View.OnClickListener, View.OnFocusChangeListener
+{
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private static final String URL = "http://10.0.2.2:8080/Blowdown/UserInfo/Login";
     private static final int SHOW_RESPONSE = 0;
+    //6~12位字母数字组合
+    private static final String REGEXUSERNAME = "([a-zA-Z0-9]{6,12})";
+    //首位不能是数字,不能全为数字或字母,6~16位
+    private static final String REGEXPASSWORD = "^(?![0-9])(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$";
 
     private String mParam1;
     private String mParam2;
-    private boolean m_userNameFlag = false;
-    private boolean m_passwordFlag = false;
 
     private View m_userView;
     private EditText m_editText_userName;
@@ -59,11 +53,11 @@ public class UserFragment extends Fragment implements View.OnClickListener, View
     private Button m_btn_register;
     private Button m_btn_forget;
     private ProgressBar m_loginProgressBar;
-    private TextView textView;
 
     private OnFragmentInteractionListener mListener;
 
-    public UserFragment() {
+    public UserFragment()
+    {
     }
 
     /**
@@ -75,7 +69,8 @@ public class UserFragment extends Fragment implements View.OnClickListener, View
      * @return A new instance of fragment UserFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static UserFragment newInstance(String param1, String param2) {
+    public static UserFragment newInstance(String param1, String param2)
+    {
         UserFragment fragment = new UserFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
@@ -90,9 +85,11 @@ public class UserFragment extends Fragment implements View.OnClickListener, View
      * @param savedInstanceState
      */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+        if(getArguments() != null)
+        {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
@@ -107,28 +104,33 @@ public class UserFragment extends Fragment implements View.OnClickListener, View
      * @return
      */
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         m_userView = inflater.inflate(R.layout.fragment_user, container, false);
         InitData();
         return m_userView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
+    public void onButtonPressed(Uri uri)
+    {
+        if(mListener != null)
+        {
             mListener.onFragmentInteraction(uri);
         }
     }
 
     @Override
-    public void onAttach(Context context) {
+    public void onAttach(Context context)
+    {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
+        if(context instanceof OnFragmentInteractionListener)
+        {
             mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+        }
+        else
+        {
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
         }
     }
 
@@ -136,72 +138,100 @@ public class UserFragment extends Fragment implements View.OnClickListener, View
      * 该Fragment从Activity删除/替换时回调该方法,onDestroy()执行后一定会执行该方法,且只调用一次
      */
     @Override
-    public void onDetach() {
+    public void onDetach()
+    {
         super.onDetach();
         mListener = null;
     }
 
     @Override
-    public void onClick(View v) {
-        if (R.id.btn_login == v.getId()) {
-
+    public void onClick(View v)
+    {
+        if(R.id.btn_login == v.getId())
+        {
+            //隐藏键盘
             InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-            if (imm.isActive()) {
+            if(imm.isActive())
+            {
                 imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
             }
-
-            if (!m_userNameFlag) {
+            if(Pattern.matches(REGEXUSERNAME, m_editText_userName.getText().toString()))
+            {
+                m_editText_userName.setError(null);
+            }
+            else
+            {
                 m_editText_userName.setError("用户名非法");
             }
-            if (!m_passwordFlag) {
+            if(Pattern.matches(REGEXPASSWORD, m_editText_password.getText().toString()))
+            {
+                m_editText_password.setError(null);
+            }
+            else
+            {
                 m_editText_password.setError("密码非法");
             }
-            if (m_userNameFlag && m_passwordFlag) {
+            if(m_editText_userName.getError() == null && m_editText_password.getError() == null)
+            {
                 Login();
             }
-        } else if (R.id.btn_register == v.getId()) {
-        } else if (R.id.btn_forget == v.getId()) {
+        }
+        else if(R.id.btn_register == v.getId())
+        {
+        }
+        else if(R.id.btn_forget == v.getId())
+        {
         }
     }
 
     @Override
-    public void onFocusChange(View v, boolean hasFocus) {
-        if (R.id.editTextUserName == v.getId()) {
-            if (hasFocus) {
+    public void onFocusChange(View v, boolean hasFocus)
+    {
+        if(R.id.editTextUserName == v.getId())
+        {
+            if(hasFocus)
+            {
                 m_editText_userName.setError(null);
-            } else {
-                String regex = "\\w{3,12}";
-                if (Pattern.matches(regex, m_editText_userName.getText().toString())) {
+            }
+            else
+            {
+                if(Pattern.matches(REGEXUSERNAME, m_editText_userName.getText().toString()))
+                {
                     m_editText_userName.setError(null);
-                    m_userNameFlag = true;
-                } else {
-                    m_userNameFlag = false;
+                }
+                else
+                {
                     m_editText_userName.setError("用户名非法");
                 }
             }
         }
-        if (R.id.editTextPasswrod == v.getId()) {
-            if (hasFocus) {
+        if(R.id.editTextPasswrod == v.getId())
+        {
+            if(hasFocus)
+            {
                 m_editText_password.setError(null);
-            } else {
-                //首位不能是数字,不能全为数字或字母,6~16位
-                String regex = "^(?![0-9])(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$";
-                if (Pattern.matches(regex, m_editText_password.getText().toString())) {
+            }
+            else
+            {
+                if(Pattern.matches(REGEXPASSWORD, m_editText_password.getText().toString()))
+                {
                     m_editText_password.setError(null);
-                    m_passwordFlag = true;
-                } else {
-                    m_passwordFlag = false;
+                }
+                else
+                {
                     m_editText_password.setError("密码非法");
                 }
             }
         }
     }
 
-    public interface OnFragmentInteractionListener {
+    public interface OnFragmentInteractionListener
+    {
         void onFragmentInteraction(Uri uri);
     }
 
-    private void IsLogining() {
+    private void IsLogining()
+    {
         m_loginProgressBar.setVisibility(View.VISIBLE);
         m_editText_userName.setEnabled(false);
         m_editText_password.setEnabled(false);
@@ -210,14 +240,17 @@ public class UserFragment extends Fragment implements View.OnClickListener, View
         m_btn_register.setEnabled(false);
     }
 
-    private Handler handler = new Handler() {
+    private Handler handler = new Handler()
+    {
         @Override
-        public void handleMessage(Message message) {
+        public void handleMessage(Message message)
+        {
             super.handleMessage(message);
-            switch (message.what) {
+            switch(message.what)
+            {
                 case SHOW_RESPONSE:
                     String responseStr = (String) message.obj;
-                    textView.setText(responseStr);
+                    IsLoginEd();
                     break;
             }
         }
@@ -226,10 +259,13 @@ public class UserFragment extends Fragment implements View.OnClickListener, View
     /**
      * 发送网络请求,在里面开启线程
      */
-    private void SendWithHttpClient() {
-        new Thread(new Runnable() {
+    private void SendWithHttpClient()
+    {
+        new Thread(new Runnable()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
                 Map<String, String> map = new HashMap<>();
                 map.put("m_userName", m_editText_userName.getText().toString());
                 map.put("m_password", m_editText_password.getText().toString());
@@ -251,15 +287,17 @@ public class UserFragment extends Fragment implements View.OnClickListener, View
         m_btn_login.setEnabled(true);
         m_btn_forget.setEnabled(true);
         m_btn_register.setEnabled(true);
-        //TODO:跳转至
+        //TODO:跳转至设备页面
     }
 
-    private void Login() {
+    private void Login()
+    {
         IsLogining();
         SendWithHttpClient();
     }
 
-    private void InitData() {
+    private void InitData()
+    {
         m_editText_userName = m_userView.findViewById(R.id.editTextUserName);
         m_editText_userName.setOnFocusChangeListener(this);
         m_editText_password = m_userView.findViewById(R.id.editTextPasswrod);
@@ -271,6 +309,5 @@ public class UserFragment extends Fragment implements View.OnClickListener, View
         m_btn_forget = m_userView.findViewById(R.id.btn_forget);
         m_btn_forget.setOnClickListener(this);
         m_loginProgressBar = m_userView.findViewById(R.id.progressBar);
-        textView = m_userView.findViewById(R.id.textView);
     }
 }
