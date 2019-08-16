e9d53ff45ab434d9a24c27794c601d4b7fe63bdd
QQ652276536 <652276536@qq.com>
2019-08-16 11:41:13 +0800

    11:41

--- app/src/main/java/com/zistone/blowdown_app/fragment/UserFragment.java
+++ app/src/main/java/com/zistone/blowdown_app/fragment/UserFragment.java
@@ -105,8 +105,7 @@ public class UserFragment extends Fragment
         {
             for(Fragment fragment : fragmentList)
             {
-                //if(!"userInfoFragment".equals(fragment.getTag()))
-                if(!"forgetFragment".equals(fragment.getTag()))
+                if(!"userInfoFragment".equals(fragment.getTag()))
                 {
                     getChildFragmentManager().beginTransaction().hide(fragment).commitNow();
                 }
@@ -120,7 +119,8 @@ public class UserFragment extends Fragment
         {
             for(Fragment fragment : fragmentList)
             {
-                if(!"loginFragment".equals(fragment.getTag()))
+                //if(!"loginFragment".equals(fragment.getTag()))
+                if(!"userInfoFragment".equals(fragment.getTag()))
                 {
                     getChildFragmentManager().beginTransaction().hide(fragment).commitNow();
                 }
